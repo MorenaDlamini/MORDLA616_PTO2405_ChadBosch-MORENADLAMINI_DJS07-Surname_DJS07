3afec793001fff392359e67588583a6ec020b213
MorenaDlamini <dlaminimorena@gmail.com>
2025-03-02 15:45:33 +0200

    Added App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Meme from './components/Meme';
 
+/**
+ * Root component of the Meme Generator application.
+ * Renders the Header and Meme components.
+ * 
+ * @component
+ * @returns {JSX.Element} The rendered App component.
+ */
 function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    );
 }
 
-export default App
+export default App;
